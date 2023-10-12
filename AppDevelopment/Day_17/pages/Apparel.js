311f9b590f8bf73543f4e09350d5ae5c872bb037
Nandhagopal <120004599+Nandha465@users.noreply.github.com>
2023-10-12 12:26:24 +0530

    Add files via upload

---
+++ AppDevelopment/Day_17/pages/Apparel.js
@@ -0,0 +1,12 @@
+import Apparel from "../components/productsPages/Apparel";
+import MenuNavBar from "../components/menuNavBar/MenuNavBar";
+const ApparelPage = () => {
+  return (
+    <>
+    <MenuNavBar />
+      <Apparel />;
+    </>
+  );
+};
+
+export default ApparelPage;
---
+++ AppDevelopment/Day_17/pages/RootLayout.js
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../components/footer/Footer";
+import ScrollToTop from "../components/UI/ScrollToTop";
+// import MenuNavBar from "../components/menuNavBar/MenuNavBar";
+
+const RootLayout = () => {
+  return (
+    <>
+    <main>
+    <ScrollToTop>
+    <Outlet />
+        </ScrollToTop>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default RootLayout;
+
+    // <MenuNavBar />
